2a10738bcaf33c15a7c45aa122f296dd6144a6c7
Steven <Hayden>
2019-11-09 03:23:46 -0600

    Added API Helper file

---
+++ Doodle/Utility/GoogleVisionApiData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Drawing;
+using Newtonsoft.Json;
+
+namespace Doodle.Utility
+{
+    public class GoogleVisionApiData
+    {
+
+        public float GetImageData(string Term, string ImagePath)
+        {
+            return 0.0f;
+        }
+
+    }
+
+}
